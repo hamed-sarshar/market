c4284d36750ed81a5e95e098410b020f98be6930
U13112 <hamedwebpro@gmail.com>
2022-03-10 10:27:59 +0330

    commit messageasds

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,9 +1,10 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 
 const Test = () => {
     return (
         <div>
-            
+            <Typography>Is Test</Typography>
         </div>
     );
 }
