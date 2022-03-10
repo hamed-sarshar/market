7c9d90285cdcf93c093fa4995048bfa66e123724
U13112 <hamedwebpro@gmail.com>
2022-03-10 10:55:31 +0330

    commit messageasds

--- src/App.jsx
+++ src/App.jsx
@@ -57,37 +57,24 @@ export default function App(props) {
     return (
         <React.Fragment>
             <SnackbarProvider maxSnack={2} autoHideDuration={1000}>
-                <Routes>
-                    <Route path="/market/" element={<Home/>}/>
-                            <Route path="/market/buys" element={<Buys/>}/>
-                            <Route path="/market/archives" element={<Archives/>}/>
-                            <Route path="/market/showArchive" element={<ShowArchive/>}/>
-                            <Route path="/market/test" element={<Test/>}/>
-                            <Route path="/market/notFound" element={<NotFound/>}/>
-                            <Route path="*" element={<NotFound/>}/>
-                        </Routes>
+                <NavBar/>
+                <span id="back-to-top-anchor"/>
+                    <Routes>
+                        <Route path="/market/" element={<Home/>}/>
+                        <Route path="/market/buys" element={<Buys/>}/>
+                        <Route path="/market/archives" element={<Archives/>}/>
+                        <Route path="/market/showArchive" element={<ShowArchive/>}/>
+                        <Route path="/market/test" element={<Test/>}/>
+                        <Route path="/market/notFound" element={<NotFound/>}/>
+                        <Route path="*" element={<NotFound/>}/>
+                    </Routes>
+                <Footer/>
+                <ScrollTop {...props}>
+                    <Fab color="secondary" size="small" aria-label="scroll back to top">
+                        <KeyboardArrowUpIcon />
+                    </Fab>
+                </ScrollTop>
             </SnackbarProvider>
         </React.Fragment>
-        // <React.Fragment>
-        //     <SnackbarProvider maxSnack={2} autoHideDuration={1000}>
-        //         <NavBar/>
-        //         <span id="back-to-top-anchor"/>
-        //             {/* <Routes>
-        //                 <Route path="/market/" element={<Home/>}/>
-        //                 <Route path="/market/buys" element={<Buys/>}/>
-        //                 <Route path="/market/archives" element={<Archives/>}/>
-        //                 <Route path="/market/showArchive" element={<ShowArchive/>}/>
-        //                 <Route path="/market/test" element={<Test/>}/>
-        //                 <Route path="/market/notFound" element={<NotFound/>}/>
-        //                 <Route path="*" element={<NotFound/>}/>
-        //             </Routes> */}
-        //         <Footer/>
-        //         <ScrollTop {...props}>
-        //             <Fab color="secondary" size="small" aria-label="scroll back to top">
-        //                 <KeyboardArrowUpIcon />
-        //             </Fab>
-        //         </ScrollTop>
-        //     </SnackbarProvider>
-        // </React.Fragment>
     );
 }
